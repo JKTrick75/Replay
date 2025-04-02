b0abf64e0d12c531d1c7b4544f77d817653337a7
JKTrick75 <david.borde75@gmail.com>
2025-04-02 16:21:24 +0200

    auth html, css, aspecte visual i canvi de login/register funcional

--- module/auth/model/auth.js
+++ module/auth/model/auth.js
@@ -1,10 +1,24 @@
 function click_login() {
     $('#login_btn').on('click', function () {
         window.location.href = 'index.php?page=controller_auth&op=list';
+        $('.register_auth').hide();
     });
+    $('.toggle_auth_login').on('click', function () {
+        $('.login_auth').hide();
+        $('.register_auth').show();
+    });
+    $('.toggle_auth_register').on('click', function () {
+        $('.register_auth').hide();
+        $('.login_auth').show();
+    });
+}
+
+function ocultar_elementos(){
+    $('.register_auth').hide();
 }
 
 $(document).ready(function () {
     click_login();
+    ocultar_elementos();
     // console.log("Bienvenido al Auth");
 });
